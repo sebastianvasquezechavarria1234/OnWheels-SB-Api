be2f7bb4ce810a321403fd754f29557c4154d9f7
sebastianvasquezechavarria1234 <sebitas221314@gmail.com>
2025-06-03 14:47:51 -0500

    ahora si

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ app.use(bodyParser.json());
 
 
 // Conexión simple y compatible
+// Conexión sin DNS SRV
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+    directConnection: true
+  })
   .then(() => console.log("✅ Conectado a MongoDB Atlas"))
   .catch((err) => console.error("❌ Error al conectar a MongoDB:", err));
 // ======================== CRUD USUARIOS ========================
